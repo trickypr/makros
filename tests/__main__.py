7b1bd13b68af2bfd8606419d26882db80b84da11
trickypr <trickypr@icloud.com>
2022-07-25 20:04:09 +1000

    💄 Clean up test formatting

--- tests/__main__.py
+++ tests/__main__.py
@@ -3,8 +3,11 @@ from typing import List
 from os import listdir
 from os.path import join, isfile
 import os
-from makros.utils import progressBar
 
+from rich.progress import track
+from rich import print
+from rich.console import Console
+from rich.markdown import Markdown
 
 def get_files(folder_name: str) -> List[str]:
     all_items = [join(folder_name, item) for item in listdir(folder_name)]
@@ -13,12 +16,12 @@ def get_files(folder_name: str) -> List[str]:
     return files
 
 
-print("Preparing for pytest")
-print("====================")
+print("[bold]Preparing for pytest")
+print()
 
 files = [file for file in get_files('./tests/macros')]
 
-for file_name in progressBar(files):
+for file_name in track(files, description="Generating tests"):
     if not file_name.endswith('.mpy'):
         continue
 
@@ -40,8 +43,9 @@ def test_answer():
     run_python_file(['{file_name.replace('.mpy', '.py')}'])
 ''')
 
-print("Installing global modules")
-print("====================")
+print()
+print("[bold]Installing global modules")
+print()
 
 subprocess.run('''cd tests/macros/global
 pip install -e .''',
@@ -49,13 +53,18 @@ pip install -e .''',
                check=True,
                executable='/bin/sh')
 
-print(
-    'Running pytest, if you get any errors make sure you are in a poetry shell'
-)
-print('\t| poetry shell')
 print()
+console = Console()
+md = Markdown("""
+Running pytest. If you get any errors or warnings, this might be because you are
+not in a poetry shell. Try starting the poetry shell:
 
-print(os.getcwd())
+```
+poetry shell
+```
+""")
+console.print(md)
+print()
 
 subprocess.run(f'pytest --cov={os.getcwd()} --cov-report xml tests/',
                shell=True)
