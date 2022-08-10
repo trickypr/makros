d555fc141364e76e2369958c9cfcdd4a824b1cab
trickypr <trickypr@icloud.com>
2022-08-10 17:49:51 +1000

    📝 Docs for cli

--- makros/cli.py
+++ makros/cli.py
@@ -4,7 +4,8 @@ import sys
 
 from makros import translate_folder
 
-
+# Poetry will bind the CLI to a function rather than a file and pass the
+# arguments via function parameters
 def cli(args=None):
     cli_parser = argparse.ArgumentParser(
         description=
@@ -16,18 +17,28 @@ def cli(args=None):
                             action="store_true")
     args = cli_parser.parse_args(args)
 
+    # To ensure that test coverage is correctly supported, we need to run a 
+    # specific function.
+    #
+    # We do not enable this by default, because I do not want to depend on
+    # the coverage modules in the final build
     if args.coverage:
+        print("Coverage requires the `coverage` module to be installed to function")
+        import coverage
         print(
             'Coverage: Remember to set `COVERAGE_PROCESS_START` env variable')
-        import coverage
         coverage.process_startup()
 
     args_path = args.file
+
+    # If the provided path is not a file, we will assume it is a folder and use
+    # the __main__.py file in it.
     if not pathlib.Path(args_path).is_file():
         args_path = pathlib.Path(args_path).joinpath("__main__.py")
         args_path = str(args_path)
 
-    current_file = str(pathlib.Path(args_path).absolute())
+    # Get the absolute path to the current file and its containing folder
+    current_file = pathlib.Path(args_path).absolute()
     current_folder = pathlib.Path(current_file).parent.absolute()
 
     translate_folder(current_folder)
