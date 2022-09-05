f7d141481ec6d33a97b39e1ee99562ba0843f2f9
trickypr <trickypr@icloud.com>
2022-09-05 16:36:13 +1000

    📝 Reference some stolen code

--- makros/registration/macro_def.py
+++ makros/registration/macro_def.py
@@ -13,7 +13,8 @@ class MacroDef:
         self.macro_name = macro_name
         self.trigger_token = trigger_token
         self.parser_file_location = parser_file_location
-
+        
+        # Stolen from stack overflow: https://stackoverflow.com/questions/41861427/python-3-5-how-to-dynamically-import-a-module-given-the-full-file-path-in-the
         spec = _importlib_util.spec_from_file_location(
             macro_name, parser_file_location)
         module = _importlib_util.module_from_spec(spec)
